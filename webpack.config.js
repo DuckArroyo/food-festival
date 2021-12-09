cada552bc7bc609d9e769ace81df42471119bcec
Rafael <developerduckarroyo@gmail.com>
2021-12-08 20:06:26 -0800

    removed unsued files after building webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 //provides access to Node.js via npm modules.
 const path = require("path");
 
+//import webpack methods
+const webpack = require("webpack");
+
 //https://webpack.js.org/configuration/mode/
 module.exports = {
   //The entry point is the root of the bundle and the beginning of the dependency graph, so give it the relative path to the client's code.
@@ -15,5 +18,10 @@ module.exports = {
   //By default, webpack wants to run in production mode. In this mode, webpack will minify our code for us automatically, along with some other nice additions. We want our code to run in development mode
   mode: "development",
   //from class
-  //plugins: [new BundlwAnalyzerPlugin()],
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+  ],
 };
